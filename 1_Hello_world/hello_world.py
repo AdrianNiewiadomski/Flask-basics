3743a8ad5262fc875a5d0d3cf9f120c95613aa56
AdrianNiewiadomski <kontakt@adrianniewiadomski.pl>
2021-06-08 22:35:15 +0200

    The first page with Flask has been created.

--- 1_Hello_world/hello_world.py
+++ 1_Hello_world/hello_world.py
@@ -5,12 +5,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
-# 3. Define your page
+# 3. Define an URL of your page
 @app.route('/')
 def display_hello_world():
+    # 4. Specify the content of the returned "page"
     return 'Hello world!'
 
 
 if __name__ == "__main__":
-    # 4. Run the application and open http://127.0.0.1:5000/ with your browser
+    # 5. Run the application and open http://127.0.0.1:5000/ with your browser
     app.run()
