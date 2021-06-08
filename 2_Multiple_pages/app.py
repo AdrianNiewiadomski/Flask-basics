db7da607bb1adeb81f1f55fcc2f2d3c08316f1e1
AdrianNiewiadomski <kontakt@adrianniewiadomski.pl>
2021-06-08 22:58:45 +0200

    The lesson about multiple pages has been added.

---
+++ 2_Multiple_pages/app.py
@@ -0,0 +1,59 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/first_page")
+def display_first_page():
+    # This will be displayed at http://127.0.0.1:5000/first_page
+    # Notice that the HTML tags may be returned.
+    return "<h1>This is the first page!</h1>"
+
+
+@app.route("/second_page")
+def display_second_page():
+    # This will be displayed at http://127.0.0.1:5000/second_page
+    # The returned string may contain JavaScript code as well.
+    return """
+        <h1>This is the second page!</h1>
+        <div id="clock">00 : 00</div>
+
+        <script type="text/javascript">
+            var hours = 0;
+            var minutes = 0;
+            var seconds = 0;
+
+            function formatTime(){
+                if(hours<10)
+                    hours = "0" + hours;
+                if(minutes<10)
+                    minutes = "0" + minutes;
+                if(seconds<10)
+                    seconds = "0" + seconds;
+            }
+
+            function setClock(){
+                var dateObject = new Date();
+                hours = dateObject.getHours();
+                minutes = dateObject.getMinutes();
+                seconds = dateObject.getSeconds();
+
+                formatTime();
+
+                document.getElementById('clock').innerHTML =
+                    hours
+                    + ' : '
+                    + minutes
+                    + '<sub>'
+                    + seconds
+                    + '</sub>';
+                setTimeout('setClock();', 1000);
+            }
+            
+            setClock();
+        </script>
+    """
+
+
+if __name__ == "__main__":
+    app.run()
