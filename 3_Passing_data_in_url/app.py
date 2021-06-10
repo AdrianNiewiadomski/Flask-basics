25afef21434905bb0f6a2437eabadd71eb209ca9
AdrianNiewiadomski <kontakt@adrianniewiadomski.pl>
2021-06-10 21:51:44 +0200

    The lesson about passing data in url has been added.

--- 3_Passing_data_in_url/app.py
+++ 3_Passing_data_in_url/app.py
@@ -3,14 +3,17 @@ from flask import Flask
 app = Flask(__name__)
 
 
+# Now, to perform a subtraction, a user has to enter two numbers in the address filed.
 @app.route("/subtraction/<minuend>/<subtrahend>")
 def perform_subtraction(minuend, subtrahend):
     try:
         a = float(minuend)
         b = float(subtrahend)
         return str(a - b)
+
+    # If the entered values are not numeric, the exception will be raised.
     except ValueError:
-        return "<h1>Error!</h1><p>An invalid number has been entered..</p>"
+        return "<h1>Error!</h1><p>An invalid number has been entered.</p>"
 
 
 if __name__ == "__main__":
