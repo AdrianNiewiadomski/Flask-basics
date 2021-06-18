31f0fb690ab7b1d671d08b1acc0d5e5cfd8e1384
AdrianNiewiadomski <kontakt@adrianniewiadomski.pl>
2021-06-18 22:03:18 +0200

    Lesson about SQLAlchemy has been added.

--- 6_Using_SQLAlchemy/app.py
+++ 6_Using_SQLAlchemy/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+# Add this import to calm down PyCharm Code Inspection ;)
 from sqlalchemy import Column, Integer, String
+
 
 app = Flask(__name__)
 
@@ -9,13 +11,16 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books.sqlite3"
 # The URI points to the place where a database is located. It is currently in the same directory as the script.
 
+
 db = SQLAlchemy(app)
 
 
 class Book(db.Model):
+    # We define table columns with their names and data types. The table should have a primary key.
     id = Column("id", Integer, primary_key=True)
+    # The author's name cannot be null.
     author = Column("author", String(20), nullable=False)
-    # If we do not define name of the column the variable name will be taken.
+    # If we do not define the name of the column, the variable name will be taken.
     title = Column(String(100), nullable=False)
     published_date = Column(Integer)
 
@@ -29,41 +34,38 @@ class Book(db.Model):
 
 
 def insert_book(author, title, published_date):
-    book = Book(author, title, published_date)
-    db.session.add(book)
+    # To insert a record into the database you have to add a record and commit it.
+    db.session.add(Book(author, title, published_date))
     db.session.commit()
 
 
 def select_books_by_author(author):
-    books = Book.query.filter_by(author=author).all()
-    for book in books:
-        print('book: ', book)
-    return books
+    # The select query takes the following form with SQLAlchemy:
+    return Book.query.filter_by(author=author).all()
 
 
 def select_all_books():
-    books = Book.query.all()
-    for book in books:
-        print('book: ', book)
-    return books
+    return Book.query.all()
 
 
-@app.route("/")
-@app.route("/<author>")
-def display_books(author=None):
-    books = None
-    if author:
-        books = select_books_by_author(author)
+@app.route("/books")
+def display_books():
+    # The data
+    if "author" in request.args.keys():
+        return render_template("index.html", books=select_books_by_author(request.args.get("author")))
     else:
-        books = select_all_books()
-    return render_template("index.html", books=books)
+        return render_template("index.html", books=select_all_books())
 
 
 if __name__ == "__main__":
     # This will create a database if it does not exist.
     db.create_all()
 
-    insert_book("Tolkien", "Hobbit", 2004)
-    insert_book("Rowling", "Harry Potter", 1997)
+    # Add some books to the table if it is empty.
+    if len(select_all_books()) == 0:
+        insert_book("Tolkien", "Hobbit", 2004)
+        insert_book("Rowling", "Harry Potter", 1997)
+        insert_book("Martin", "A Game of Thrones", 1996)
+        insert_book("Rothfuss", "The Name of the Wind", 2007)
 
     app.run()
